abe61337afd1849e58bb2cafec2c7f01be010a88
crenteriam <renteman@hotmail.com>
2019-02-26 16:41:05 -0500

    funcion rnc

--- R/sig_caminos_extrae.R
+++ R/sig_caminos_extrae.R
@@ -1,40 +1,41 @@
 #' Red Nacional de Caminos II
 #'
-#' @param year An`o de la version de la Red Nacional de Caminos (disponibles 2016, 2017, 2018)
+#' @param . Objeto (inserta el objeto creado con sig_caminos())
+#' @param year An~o de la version de la Red Nacional de Caminos (disponibles 2014-2018)
+#' @param conjunto Capa de Shapefile a descargar. Disponibles: estructura, localidad, maniobra_prohibida, plaza_cobro, poste_de_referencia, puente, red_vial, sitio_de_interes, tarifas, transbordador, tred_localidad, tred_sitio_de_interes, runion.
+#'
 
-sig_caminos_extrae <- function(. = NA, conjunto = NA){
+sig_caminos_extrae <- function(. = NA, year = NA, conjunto = NA){
   library(rgdal)
   # Generales -----------------------------------------------------------------
-  if (year == NA){shell.exec("https://www.inegi.org.mx/temas/mapas/viascomunicacion/")}
-
-  # Informacion de la version
-  message("
-          \rRed Nacional de Caminos
-          \rAños disponible: 2018, 2017 y 2016.
-          \r\n")
+  #if (conjunto == ){shell.exec("https://www.inegi.org.mx/temas/mapas/viascomunicacion/")}
 
 # Informacion de la funcion
-message(" sig_caminos_extrae() Versión 1.0.
-          \rConjuntos de datos (shapeflies) disponibles:
-          \n
-          \r2018: estructura, localidad, maniobra_prohibida, plaza_cobro,
-          \rposte_de_referencia, puente, red_vial, sitip_de_interes,
-          \rtarifas, transbordador, tred_localidad, tred_sitio_de_interes,
-          \runion.
-          \n
-          \rPara consultar el diccionario de datos, escribe:
-          \rsig_caminos_extrae(conjunto = \"diccionario\")
-          \r\n")
+message("sig_caminos_extrae() Versión 1.0.
+         \rConjuntos de datos (shapeflies) disponibles:
+         \n
+         \r2018: estructura, localidad, maniobra_prohibida, plaza_cobro,
+         \rposte_de_referencia, puente, red_vial, sitio_de_interes,
+         \rtarifas, transbordador, tred_localidad, tred_sitio_de_interes,
+         \runion.
+         \r\n")
+
+if      (year == 2018) {rutaarchivo = ""}
+else if (year == 2017) {rutaarchivo = ""}
+else if (year == 2016) {rutaarchivo = ""}
+else if (year == 2015) {rutaarchivo = "./red_nacional_de_caminos_2015/conjunto_de_datos"}
+else if (year == 2014) {rutaarchivo = ""}
 
   # Unzip and open
+  print("Descomprimiendo archivo...")
   temp_sigdir = unzip(.)
-  list_sigfiles = as.list(temp_sigdir)
-
-  #
+  #list_sigfiles = as.list(temp_sigdir)
   #path_conjunto  = paste0("./conjunto_de_datos/", "localidad")
-  #lista_conjunto = Filter(function (x) str_detect(x, path_conjunto), .)
-  #lista_conjunto = str_replace_all(lista_conjunto, "^./(.+)/", "")
-  map_output = readOGR(dsn = "./red_nacional_de_caminos_2015/conjunto_de_datos/", layer = conjunto)
+  #lista_conjunto = Filter(function (x) str_detect(x, path_conjunto), data)
+  #temp = str_replace_all(temp, "^./(.+)/", "./folder/")
+
+  print("Obteniendo Shapefile...")
+  map_outputs = readOGR(dsn = rutaarchivo, layer = conjunto)
   return(map_output)
 
 } # End of Function
