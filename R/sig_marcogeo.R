863686e317edc0298c3b6a900be4c7b500000de6
crenteriam <renteman@hotmail.com>
2019-03-02 16:21:54 -0500

    building

--- R/sig_marcogeo.R
+++ R/sig_marcogeo.R
@@ -22,48 +22,52 @@ if (is.na(year) & is.na(capa) & is.na(version)) {base::shell.exec("https://www.i
 
 # Links:
 url_base = "http://internet.contenidos.inegi.org.mx/contenidos/Productos/prod_serv/contenidos/espanol/bvinegi/productos/geografia/marc_geo/"
-# 1995 - "http://internet.contenidos.inegi.org.mx/contenidos/Productos/prod_serv/contenidos/espanol/bvinegi/productos/geografia/marc_geo/702825292836_s.zip"
 
-# Codigo Year
-if      (year == 1995) {codigo_year = "702825292836_s"}
-else if (year == XXXX) {codigo_year = }
-else if (year == XXXX) {codigo_year = }
-else if (year == XXXX) {codigo_year = }
-else if (year == XXXX) {codigo_year = }
-else if (year == XXXX) {codigo_year = }
-else if (year == XXXX) {codigo_year = }
+# URL por An~o
+if      (year == 1995) {url_marcogeo   = "http://internet.contenidos.inegi.org.mx/contenidos/Productos/prod_serv/contenidos/espanol/bvinegi/productos/geografia/marc_geo/702825292836_s.zip"}
+else if (year == 2000) {codigo_year = ""}
+else if (year == 2000) {codigo_year = ""}
+else if (year == 2000) {codigo_year = ""}
+else if (year == 2000) {codigo_year = ""}
+else if (year == 2000) {codigo_year = ""}
+else if (year == 2000) {codigo_year = ""}
 
 # Codigo de la Capa
-if (capa == entidades)  {codigo_capa = "mge"}
-if (capa == municipios) {codigo_capa = "mgm"}
-if (capa == ageb)       {codigo_capa = "mgau"}
-if (capa == urbano)     {codigo_capa = "mgr"}
-if (capa == rural)      {codigo_capa = "mgu"}
+if (capa == "entidades"){
+    codigo_capa = "mge"
+    nombre_capa = "Entidades"
+    }
+if (capa == "municipios"){
+    codigo_capa = "mgm"
+    nombre_capa = "Municipios"
+    }
+if (capa == "ageb")       {codigo_capa = "mgau"}
+if (capa == "urbano")     {codigo_capa = "mgr"}
+if (capa == "rural")      {codigo_capa = "mgu"}
 
-
-#Download File
-  url.marcogeo = "http://internet.contenidos.inegi.org.mx/contenidos/Productos/prod_serv/contenidos/espanol/bvinegi/productos/geografia/marc_geo/702825292836_s.zip"
+  # Desgargas  ------------------------------------------------------------------
+  # Download File
   tempmarcogeo = tempfile()
-  download.file(url.marcogeo, tempmarcogeo)
+  download.file(url_marcogeo, tempmarcogeo)
 
   # Unzip and open
   zipdir = tempdir()
-  zipdir_entidades  = tempdir()
-  zipdir_municipios = tempdir()
+  zipdir_level  = tempdir()
   utils::unzip(tempmarcogeo, exdir = zipdir)
 
   # 1995
-  utils::unzip(paste0(zipdir, "\\\\mge1995.zip"), exdir = zipdir_entidades)
-  utils::unzip(paste0(zipdir, "\\\\mgm1995.zip"), exdir = zipdir_municipios)
-  map_output = readOGR(dsn = zipdir_entidades, layer = "Entidades_1995")
-  map_output = readOGR(dsn = zipdir_municipios, layer = "Municipios_1995")
+  if (year == 1995) {utils::unzip(paste0(zipdir, paste0("\\\\", codigo_capa, year, ".zip")), exdir = zipdir_level)}
+  else {}
 
   # 2000
   # agebs_urb_2000
-  utils::unzip(paste0(zipdir, "\\\\mgau2000.zip"), exdir = zipdir_entidades)
-  utils::unzip(paste0(zipdir, "\\\\mge2000.zip"), exdir = zipdir_entidades)
-  utils::unzip(paste0(zipdir, "\\\\mgm2000.zip"), exdir = zipdir_municipios)
-  map_output = readOGR(dsn = zipdir_entidades, layer = "Entidades_1995")
-  map_output = readOGR(dsn = zipdir_municipios, layer = "Municipios_1995")
+  utils::unzip(paste0(zipdir, paste0("\\\\", codigo_capa, year, ".zip")), exdir = zipdir_level)
+  #utils::unzip(paste0(zipdir, "\\\\mge2000.zip"), exdir = zipdir_entidades)
+  #utils::unzip(paste0(zipdir, "\\\\mgm2000.zip"), exdir = zipdir_municipios)
 
+  # Extract Map
+  if (year == 1995) {map_output = readOGR(dsn = zipdir_level, layer = paste0(nombre_capa, "_", year))}
+  #map_output = readOGR(dsn = zipdir_municipios, layer = "Municipios_1995")
+
+  return(map_output)
 } # End of Function
