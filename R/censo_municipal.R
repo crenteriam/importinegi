55aaa01e2f38bb654cc4ac00a51aad919f0e27e5
crenteriam <renteman@hotmail.com>
2019-03-11 20:14:27 -0400

    developing

--- R/censo_municipal.R
+++ R/censo_municipal.R
@@ -1,6 +1,5 @@
 CensoMunicipal = function(fuente = NA, year = NA, datos = NA){
 library(foreign)
-  library(rio)
 
 # Open Metadata
 if (is.na(fuente) & is.na(year) & is.na(datos)) {shell.exec("http://www3.inegi.org.mx/rnm/index.php/catalog/208")}
@@ -18,6 +17,12 @@ if      (year == 2011) {
     url.datos.aprec = "apmd_recursos_cngmd"
     url.datos.aptra = "apmd_tramites_cngmd"
     url.datos.aptrn = "apmd_transparencia_cngmd"
+    url.datos.speje = "sp_ejercicio_cngmd"
+    url.datos.spinf = "sp_infraestructura_cngmd"
+    url.datos.sprec = "sp_recursos_cngmd"
+    url.datos.jmeje = "jm_ejercicio_cngmd"
+    url.datos.jminf = "jm_infraestructura_cngmd"
+    url.datos.jmrec = "jm_recursos_cngmd"
     folder.comis = "/Comisiones e iniciativas de los Ayuntamientos/Base de datos/"
     folder.estru = "/Estructura de los Ayuntamientos/Base de datos/"
     folder.apest = "/Estructura organizacional/Base de datos/"
@@ -27,41 +32,144 @@ if      (year == 2011) {
     folder.aprec = "/Recursos de las Administraciones Publicas Municipales/Base de datos/"
     folder.aptra = "/Tramites y Servicios y Gobierno Electronico/Base de datos/"
     folder.aptrn = "/Transparencia y Anticorrupcion/Base de datos/"
+    folder.speje = "/Ejercicio de la Funcion/Base de datos/"
+    folder.spinf = "/Infraestructura/Base de datos/"
+    folder.sprec = "/Recursos Humanos/Base de datos/"
+    folder.jmeje = "/Ejercicio de la Funcion/Base de datos/"
+    folder.jminf = "/Infraestructura/Base de datos/"
+    folder.jmrec = "/Recursos Humanos/Base de datos/"
 }
 else if (year == 2013) {
     url.datos.comis = "Com_ini_aytto_cngmd"
     url.datos.estru = "Estruct_aytto_cngmd"
+    url.datos.apest = "estructura_organizacional_cngmd"
+    url.datos.apfun = "func_esp_cngmd"
+    url.datos.apmar = "marco_regulatorio_cngmd"
+    url.datos.appar = "participacion_ciudadana_cngmd"
+    url.datos.aprec = "recursos_cngmd"
+    url.datos.aptra = "tys_gob_elec_cngmd"
+    url.datos.aptrn = "transp_cont_int_anticor_cngmd"
+    url.datos.speje = "SP_Ejercicio_funcion_cngmd"
+    url.datos.spinf = "SP_Infraestructura_cngmd"
+    url.datos.sprec = "SP_Recursos_cngmd"
+    url.datos.jmeje = "JM_Ejercicio_funcion_cngmd"
+    url.datos.jminf = "JM_Infraestructura_cngmd"
+    url.datos.jmrec = "JM_Recursos_cngmd"
+    folder.comis = "/Com_ini_aytto/Bases_de_datos/"
+    folder.estru = "/Estruct_aytto/Bases_de_datos/"
+    folder.apest = "/Estructura_organizacional/Bases_de_datos/"
+    folder.apfun = "/Func_esp/Bases_de_datos/"
+    folder.apmar = "/Marco_regulatorio/Bases_de_datos/"
+    folder.appar = "/Participacion_ciudadana/Bases_de_datos/"
+    folder.aprec = "/Recursos/Bases_de_datos/"
+    folder.aptra = "/TyS_gob_elec/Bases_de_datos/"
+    folder.aptrn = "/Transp_cont_int_anticor/Bases_de_datos/"
+    folder.speje = "/SP_Ejercicio_funcion/Bases_de_datos/"
+    folder.spinf = "/SP_Infraestructura/Bases_de_datos/"
+    folder.sprec = "/SP_Recursos/Bases_de_datos/"
+    folder.jmeje = "/JM_Ejercicio_funcion/Bases_de_datos/"
+    folder.jminf = "/JM_Infraestructura/Bases_de_datos/"
+    folder.jmrec = "/JM_Recursos/Bases_de_datos/"
 }
 else if (year == 2015) {
   url.datos.comis = "m1/Com_ini_aytto_cngmd"
   url.datos.estru = "m1/Estruct_aytto_cngmd"
+  url.datos.apest = ""
+  url.datos.apfun = ""
+  url.datos.apmar = ""
+  url.datos.appar = ""
+  url.datos.aprec = ""
+  url.datos.aptra = ""
+  url.datos.aptrn = ""
+  url.datos.speje = ""
+  url.datos.spinf = ""
+  url.datos.sprec = ""
+  url.datos.jmeje = ""
+  url.datos.jminf = ""
+  url.datos.jmrec = ""
+  folder.comis = paste0("/Com_ini_aytto_cngmd", year, "_dbf/Bases_de_datos/")
+  folder.estru = paste0("/Estruct_aytto_cngmd", year, "_dbf/Bases_de_datos/")
+  folder.apest = "//Bases_de_datos/"
+  folder.apfun = "//Bases_de_datos/"
+  folder.apmar = "//Bases_de_datos/"
+  folder.appar = "//Bases_de_datos/"
+  folder.aprec = "//Bases_de_datos/"
+  folder.aptra = "//Bases_de_datos/"
+  folder.aptrn = "//Bases_de_datos/"
+  folder.speje = "//Bases_de_datos/"
+  folder.spinf = "//Bases_de_datos/"
+  folder.sprec = "//Bases_de_datos/"
+  folder.jmeje = "//Bases_de_datos/"
+  folder.jminf = "//Bases_de_datos/"
+  folder.jmrec = "//Bases_de_datos/"
 }
 else if (year == 2017) {
   url.datos.comis = "m1/Actividad_ayuntami_cngmd"
   url.datos.estru = "m1/Integrantes_ayuntami_cngmd"
-}
-else {}
-if      (fuente == "ayuntamiento" & datos == "comision")   {url.complemento = url.datos.comis}
-else if (fuente == "ayuntamiento" & datos == "estructura") {url.complemento = url.datos.estru}
-
-## Folders  ------------------------------------------------------------------------------------------
-if (year == 2013) {
-  folder.comis = "/Com_ini_aytto/Bases_de_datos/"
-  folder.estru = "/Estruct_aytto/Bases_de_datos/"
-}
-else if (year == 2015) {
+  url.datos.apest = ""
+  url.datos.apfun = ""
+  url.datos.apmar = ""
+  url.datos.appar = ""
+  url.datos.aprec = ""
+  url.datos.aptra = ""
+  url.datos.aptrn = ""
+  url.datos.speje = ""
+  url.datos.spinf = ""
+  url.datos.sprec = ""
+  url.datos.jmeje = ""
+  url.datos.jminf = ""
+  url.datos.jmrec = ""
   folder.comis = paste0("/Com_ini_aytto_cngmd", year, "_dbf/Bases_de_datos/")
   folder.estru = paste0("/Estruct_aytto_cngmd", year, "_dbf/Bases_de_datos/")
-}
-else if (year == 2017) {
-  folder.comis = "/Bases_Datos/CABILDOS."
-  folder.estru = "/Bases_Datos/AYUNTAMI."
+  folder.apest = "//Bases_de_datos/"
+  folder.apfun = "//Bases_de_datos/"
+  folder.apmar = "//Bases_de_datos/"
+  folder.appar = "//Bases_de_datos/"
+  folder.aprec = "//Bases_de_datos/"
+  folder.aptra = "//Bases_de_datos/"
+  folder.aptrn = "//Bases_de_datos/"
+  folder.speje = "//Bases_de_datos/"
+  folder.spinf = "//Bases_de_datos/"
+  folder.sprec = "//Bases_de_datos/"
+  folder.jmeje = "//Bases_de_datos/"
+  folder.jminf = "//Bases_de_datos/"
+  folder.jmrec = "//Bases_de_datos/"
 }
 else {}
 
-# Combine into folder.complement
-if      (fuente == "ayuntamiento" & datos == "comision")   {folder.complemento = folder.comis}
-else if (fuente == "ayuntamiento" & datos == "estructura") {folder.complemento = folder.estru}
+# URL Por fuente y datos
+if      (fuente == "ayuntamiento" & datos == "comision")     {url.complemento = url.datos.comis}
+else if (fuente == "ayuntamiento" & datos == "estructura")   {url.complemento = url.datos.estru}
+else if (fuente == "administracion" & datos == "estructura") {url.complemento = url.datos.apest}
+else if (fuente == "administracion" & datos == "funciones")  {url.complemento = url.datos.apfun}
+else if (fuente == "administracion" & datos == "marco")      {url.complemento = url.datos.apmar}
+else if (fuente == "administracion" & datos == "participacion") {url.complemento = url.datos.appar}
+else if (fuente == "administracion" & datos == "recursos")      {url.complemento = url.datos.aprec}
+else if (fuente == "administracion" & datos == "tramites")      {url.complemento = url.datos.aptra}
+else if (fuente == "administracion" & datos == "transparencia") {url.complemento = url.datos.aptrn}
+else if (fuente == "seguridad" & datos == "ejercicio")          {url.complemento = url.datos.speje}
+else if (fuente == "seguridad" & datos == "infraestructura")    {url.complemento = url.datos.spinf}
+else if (fuente == "seguridad" & datos == "recursos")           {url.complemento = url.datos.sprec}
+else if (fuente == "justicia" & datos == "ejercicio")           {url.complemento = url.datos.jmeje}
+else if (fuente == "justicia" & datos == "infraestructura")     {url.complemento = url.datos.jminf}
+else if (fuente == "justicia" & datos == "recursos")            {url.complemento = url.datos.jmrec}
+
+# Folders por fuente y datos
+if      (fuente == "ayuntamiento" & datos == "comision")       {folder.complemento = folder.comis}
+else if (fuente == "ayuntamiento" & datos == "estructura")     {folder.complemento = folder.estru}
+else if (fuente == "administracion" & datos == "estructura") {folder.complemento = folder.apest}
+else if (fuente == "administracion" & datos == "funciones")  {folder.complemento = folder.apfun}
+else if (fuente == "administracion" & datos == "marco")      {folder.complemento = folder.apmar}
+else if (fuente == "administracion" & datos == "participacion") {folder.complemento = folder.appar}
+else if (fuente == "administracion" & datos == "recursos")      {folder.complemento = folder.aprec}
+else if (fuente == "administracion" & datos == "tramites")      {folder.complemento = folder.aptra}
+else if (fuente == "administracion" & datos == "transparencia") {folder.complemento = folder.aptrn}
+else if (fuente == "seguridad" & datos == "ejercicio")          {folder.complemento = folder.speje}
+else if (fuente == "seguridad" & datos == "infraestructura")    {folder.complemento = folder.spinf}
+else if (fuente == "seguridad" & datos == "recursos")           {folder.complemento = folder.sprec}
+else if (fuente == "justicia" & datos == "ejercicio")           {folder.complemento = folder.jmeje}
+else if (fuente == "justicia" & datos == "infraestructura")     {folder.complemento = folder.jminf}
+else if (fuente == "justicia" & datos == "recursos")            {folder.complemento = folder.jmrec}
 else {}
 
 ### Descargar  -----------------------------------------------------------------------------------------
